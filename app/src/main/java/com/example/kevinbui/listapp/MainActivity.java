6583b0cea3e9396c8e603115fe0f571c9b704886
buikevin96 <buikevin96@gmail.com>
2019-01-26 17:22:45 -0500

    Presents 3 textviews within each item

--- app/src/main/java/com/example/kevinbui/listapp/MainActivity.java
+++ app/src/main/java/com/example/kevinbui/listapp/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
     ListView myListView;//Reference to listview
     String[] items;
+    String[] prices;
+    String[] descriptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
         Resources res = getResources(); // Reference to resources
         myListView = (ListView)findViewById(R.id.myListView);
-        items = res.getStringArray(R.array.items); // List of strings
+        items = res.getStringArray(R.array.items); // List of strings for items
+        prices = res.getStringArray(R.array.prices); // List of strings for prices
+        descriptions = res.getStringArray(R.array.descriptions); // List of strings for descriptions
 
-        myListView.setAdapter(new ArrayAdapter<String>(this, R.layout.my_listview_detail, items));
-
+        // Itemadapater merge 3 files into layout file
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions); // Only referencing
+        myListView.setAdapter(itemAdapter); // Use the reference
     }
 }
