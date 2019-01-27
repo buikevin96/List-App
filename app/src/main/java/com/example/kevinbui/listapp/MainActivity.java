93de193075b9003e500e21cda288ba5db141cc50
buikevin96 <buikevin96@gmail.com>
2019-01-27 15:37:39 -0500

    When item is clicked, switches the screen

--- app/src/main/java/com/example/kevinbui/listapp/MainActivity.java
+++ app/src/main/java/com/example/kevinbui/listapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.kevinbui.listapp;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -30,5 +33,17 @@ public class MainActivity extends AppCompatActivity {
         // To use new adapter, create reference to it
         ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
         myListView.setAdapter(itemAdapter);
+
+        // Go from one screen to another screen or activity
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                // Send that activity the index of the item that got clicked
+                showDetailActivity.putExtra("com.example.kevinbui.ITEM_INDEX", i);
+                startActivity(showDetailActivity); // Switch the screen when we click the item
+
+            }
+        });
     }
 }
